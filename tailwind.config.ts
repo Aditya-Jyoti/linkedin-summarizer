3e67bffcddf7b2656cf9c10ffb6e4a7c39328b2c
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-11-10 22:07:50 +0530

    chore: Add styling config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,30 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#6A0DAD',
+				secondary: '#8A2BE2',
+				accent: '#D1C4E9',
+				neutral: '#333333',
+				background: '#FFFFFF',
+				textPrimary: '#333333',
+				textSecondary: '#4B5563',
+				textOnPrimary: '#FFFFFF',
+				border: '#E0E0E0'
+			},
+			font: {
+				family: {
+					body: ['Roboto', 'sans-serif']
+				},
+				sizes: {
+					small: '0.875rem',
+					medium: '1rem',
+					large: '1.25rem',
+					xlarge: '1.5rem'
+				}
+			}
+		}
 	},
 
 	plugins: []
