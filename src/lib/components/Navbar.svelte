32cbbc48cf40e9c99665f1cd2b4ffb06d2428bc7
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-11-10 23:29:31 +0530

    feat: Make splash section

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <nav class="flex items-center justify-between px-4 py-2 shadow-md">
-	<div class="flex items-center gap-2">
+	<div class="flex flex-1 items-center gap-2">
 		<img src="/logo.png" alt="logo" class="aspect-square w-10" />
 		<span class="text-lg font-black uppercase">linkedin summarizer</span>
 	</div>
@@ -11,8 +11,11 @@
 		<a href="#about" class="text-base font-normal uppercase">about</a>
 		<a href="#pricing" class="text-base font-normal uppercase">pricing</a>
 		<a href="#contact" class="text-base font-normal uppercase">contact</a>
+	</div>
+
+	<div class="flex flex-1 justify-end">
 		<button
-			class="rounded-lg bg-primary px-4 py-1 text-base font-semibold uppercase text-textOnPrimary transition-colors duration-300 hover:bg-secondary"
+			class="w-fit rounded-lg bg-primary px-4 py-1 text-base font-semibold uppercase text-textOnPrimary transition-colors duration-300 hover:bg-secondary"
 			>sign up</button
 		>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,27 @@
 <script lang="ts"></script>
 
-<main></main>
+<main class="flex flex-col">
+	<!-- splash section -->
+	<section class="flex flex-col items-center justify-center gap-4 bg-border/25 p-2 py-12">
+		<!-- splash text -->
+		<div class="flex flex-col items-center justify-center">
+			<span class="text-center text-4xl font-medium tracking-wide">
+				Summarize and <span class="text-primary">analyze</span>
+			</span>
+			<span class="text-xl"
+				>your linkedin data with just one <span class="text-primary">click</span>
+			</span>
+
+			<button class="my-4 rounded-xl bg-neutral hover:bg-gray-700 transition-colors duration-300 px-12 py-1 text-textOnPrimary shadow-2xl"
+				>Get Started</button
+			>
+		</div>
+
+		<!-- splash image -->
+		<div class="flex gap-36">
+			<img src="/splash1.svg" alt="realtime analytics" class="aspect-square w-52" />
+			<img src="/splash2.svg" alt="growing statistics" class="aspect-square w-52" />
+			<img src="/splash3.svg" alt="visualizing projections" class="aspect-square w-52" />
+		</div>
+	</section>
+</main>
